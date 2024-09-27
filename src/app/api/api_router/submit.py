ee13ced6f713e0f9e745d90b6a89fac763c87027
Toan <nvantoan1203@gmail.com>
2024-09-27 15:50:35 +0700

    add pop up submit

--- src/app/api/api_router/submit.py
+++ src/app/api/api_router/submit.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional
 import os 
+import random
 # Khởi tạo APIRouter
 router = APIRouter()
 
@@ -11,31 +12,69 @@ router = APIRouter()
 class VideoFrameData(BaseModel):
     videoId: str
     frameId: str
-    filename: str 
-    qa: Optional[str] = Field(None, description="QA text if available")  # Trường QA là tùy chọn
+    questionId: str
+    qaType: Optional[str] = None  # Loại QA có thể không bắt buộc
+    minFrameValue: Optional[int] = None  # Min frame
+    maxFrameValue: Optional[int] = None  # Max frame
+    minQuantityValue: Optional[int] = None  # Min quantity
+    maxQuantityValue: Optional[int] = None  # Max quantity
+    randomNumber: int  # Số random phải có
+
+class Small(BaseModel):
+    questionId: str
+    text: str
 
 # Định nghĩa endpoint lưu dữ liệu CSV
 @router.post("/save-csv")
 async def save_csv(data: VideoFrameData):
+    result = []
+    VideoId = data.videoId
+    frameId = data.frameId
+    questionId = data.questionId
+    qaType = data.qaType
+    minFrameValue = data.minFrameValue
+    maxFrameValue = data.maxFrameValue
+    minQuantityValue = data.minQuantityValue
+    maxQuantityValue = data.maxQuantityValue
+    randomNumber = data.randomNumber
+
+    # Lặp qua số lần randomNumber và chọn frame ngẫu nhiên
+    for _ in range(randomNumber):
+        if qaType == 'QA':
+            random_frame = random.randint(minFrameValue, maxFrameValue)
+            number = random.randint(minQuantityValue, maxQuantityValue)
+            result.append([VideoId, frameId, random_frame, number])
+        else:
+            random_frame = random.randint(minFrameValue, maxFrameValue)
+            result.append([VideoId, frameId, random_frame])
+
+    # Tạo file CSV với tên là question_id.csv
+    file_name = f"{questionId}.csv"
+
+    # Kiểm tra nếu file đã tồn tại
+    file_exists = os.path.isfile(file_name)
+
+    with open(file_name, mode='a' if file_exists else 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Ghi dữ liệu vào file mà không có header
+        writer.writerows(result)
+
+    return {"message": "CSV file saved successfully", "file_name": file_name}
+
+
+@router.post("/save-csv-small")
+async def save_csv_small(data: Small):
+    questionId = data.questionId
+    text = data.text.split(",")
+    # Tạo file CSV với tên là question_id.csv
     
-    try:
-        # Đảm bảo dữ liệu không trống
-        if not data.videoId or not data.frameId:
-            raise HTTPException(status_code=400, detail="Missing videoId or frameId")
-        filecsv = os.path.join('result', data.filename)
-        # Mở file CSV để append dữ liệu mới
-        with open(f"{filecsv}.csv", mode="w", newline="", encoding="utf-8") as file:
-            writer = csv.writer(file)
+    file_name = f"{questionId}.csv"
 
-            # Lưu dòng mới vào CSV (tùy thuộc vào QA có được cung cấp hay không)
-            if data.qa:
-                writer.writerow([data.videoId, data.frameId, data.qa])
-            else:
-                writer.writerow([data.videoId, data.frameId])
+    # Kiểm tra nếu file đã tồn tại
+    file_exists = os.path.isfile(file_name)
 
-        # Trả về phản hồi JSON khi thành công
-        return JSONResponse(status_code=200, content={"message": "CSV data saved successfully"})
-
-    except Exception as e:
-        # Xử lý lỗi và trả về phản hồi
-        return JSONResponse(status_code=500, content={"message": f"Error saving CSV: {str(e)}"})
+    result= ([text[0], text[1], text[2], text[3]] )if len(text) == 4 else  ([text[0], text[1], text[2]])
+    with open(file_name,mode='a' if file_exists else 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Ghi dữ liệu vào file mà không có header
+        writer.writerow(result)
--- src/app/static/js/submitbtn.js
+++ src/app/static/js/submitbtn.js
@@ -3,7 +3,22 @@ export function handleSubmitButtonClick() {
     const popupContainer = document.getElementById('popup-container');
     const popupOverlay = document.getElementById('popup-overlay');
     const confirmSubmitButton = popupContainer.querySelector('#confirm-submit');
-    let isSubmitEventAttached = false;  // Biến cờ để theo dõi việc gắn sự kiện
+    const cancel = document.getElementById('cancel');
+    const Frame_id = document.getElementById('frame-id');
+    const Video_id = document.getElementById('video-id');
+
+    const randomButton = document.getElementById('random');
+    const questionIdInput = document.getElementById('question-id');
+    const qaTypeSelect = document.getElementById('QA');
+    const minValueFrameInput = document.getElementById('min-value-frame');
+    const maxValueFrameInput = document.getElementById('max-value-frame');
+    const minValueQuantityInput = document.getElementById('min-value-quantity');
+    const maxValueQuantityInput = document.getElementById('max-value-quantity');
+    const numberRandomInput = document.getElementById('Number-random');
+    const minQuantityDisplay = document.getElementById('min-quantity-value-display');
+    const maxQuantityDisplay = document.getElementById('max-quantity-value-display');
+    const minFrameDisplay = document.getElementById('min-frame-value-display');
+    const maxFrameDisplay = document.getElementById('max-frame-value-display');
 
     submitButtons.forEach(button => {
         button.addEventListener('click', function () {
@@ -13,86 +28,103 @@ export function handleSubmitButtonClick() {
             // Hiển thị popup và overlay
             popupContainer.style.display = 'block';
             popupOverlay.style.display = 'block';
+            Frame_id.value = frameId;
+            Video_id.value = videoId;
+        });
+    });
 
-            // Lấy giá trị từ phần tử filename textarea
-            const filenameInput = popupContainer.querySelector('#filename');
-            const qaCheckbox = popupContainer.querySelector('#qa-checkbox');
-            const qaTextInput = popupContainer.querySelector('#qa-text');
-            const qaInputContainer = popupContainer.querySelector('#qa-input-container');
+    // Event listener for the Random button
+    randomButton.addEventListener('click', function () {
+        const videoId = Video_id.value; 
+        const frameId = Frame_id.value
+        
 
-            // Đảm bảo trạng thái ban đầu của qaCheckbox và qaInputContainer
-            qaCheckbox.checked = false;
-            qaInputContainer.style.display = 'none';
-            filenameInput.value = '';  // Reset filename input
+        // Tạo payload
+        let payload = {
+            videoId: videoId,
+            frameId: frameId,
+            questionId: questionIdInput.value.trim(),
+            qaType: qaTypeSelect.value,
+            minFrameValue: minFrameDisplay.textContent,
+            maxFrameValue: maxFrameDisplay.textContent,
+            minQuantityValue: minQuantityDisplay.textContent,
+            maxQuantityValue: maxQuantityDisplay.textContent,
+            randomNumber: numberRandomInput.value,
+        };
 
-            // Sự kiện thay đổi hiển thị QA Text nếu QA checkbox được chọn
-            qaCheckbox.addEventListener('change', function () {
-                if (this.checked) {
-                    qaInputContainer.style.display = 'block';
-                } else {
-                    qaInputContainer.style.display = 'none';
-                }
-            });
-
-            // Kiểm tra và chỉ thêm sự kiện submit một lần
-            if (!isSubmitEventAttached) {
-                confirmSubmitButton.addEventListener('click', handleSubmit);
-                isSubmitEventAttached = true;  // Gắn cờ khi sự kiện đã được gắn
-            }
-
-            // Hàm xử lý submit
-            function handleSubmit() {
-                const filename = filenameInput.value.trim();  // Lấy giá trị từ textarea filename
-                const isQAEnabled = qaCheckbox.checked;
-                const qaText = qaTextInput.value.trim();
-
-                // Kiểm tra điều kiện bắt buộc
-                if (!videoId || !frameId || !filename) {
-                    alert('Error: Missing required fields (videoId, frameId, filename)');
-                    return;
-                }
-
-                // Tạo payload dựa trên QA checkbox
-                let payload = {
-                    videoId: videoId,
-                    frameId: frameId,
-                    filename: filename  // Đảm bảo filename được lấy từ textarea
-                };
-
-                if (isQAEnabled && qaText) {
-                    payload.qa = qaText; // Nếu QA được bật, thêm trường QA vào payload
-                }
-                // Gửi payload dạng JSON
-                fetch('http://127.0.0.1:8000/save-csv', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(payload)
-                }).then(response => {
-                    if (response.ok) {
-                        // Đặt lại form về trạng thái ban đầu
-                        filenameInput.value = '';
-                        qaCheckbox.checked = false;
-                        qaInputContainer.style.display = 'none';  
-                        qaTextInput.value = '';  
-                        popupContainer.style.display = 'none';
-                        popupOverlay.style.display = 'none';
-                        
-                    } else {
-                        alert('Error saving data!');
-                    }
-                }).catch(err => {
-                    alert('Error sending CSV data: ' + err); // Hiển thị alert khi có lỗi
-                    console.error('Error sending CSV data:', err);
-                });
-            }
-
-            // Đóng popup khi nhấp ra ngoài
-            popupOverlay.addEventListener('click', function() {
+        // Gửi payload dạng JSON
+        fetch('http://127.0.0.1:8000/save-csv', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload)
+        }).then(response => {
+            if (response.ok) {
+                questionIdInput.value = ''; 
+                minValueFrameInput.value = ''; 
+                maxValueFrameInput.value = ''; 
+                minValueQuantityInput.value = ''; 
+                maxValueQuantityInput.value = ''; 
+                numberRandomInput.value = ''; 
                 popupContainer.style.display = 'none';
                 popupOverlay.style.display = 'none';
-            });
+            } else {
+                alert('Error saving data!');
+            }
+        }).catch(err => {
+            alert('Error sending CSV data: ' + err); 
+            console.error('Error sending CSV data:', err);
         });
+
+    });
+
+    document.getElementById('insert').addEventListener('click', function() {
+        // Hiển thị bảng nhỏ khi bấm Insert
+        document.getElementById('small-popup-container').style.display = 'block';
+    });
+
+    document.getElementById('small-popup-close').addEventListener('click', function() {
+        // Đóng bảng nhỏ khi bấm nút Close
+        document.getElementById('small-popup-container').style.display = 'none';
+    });
+
+    const text_small = document.getElementById('small-text-input')
+
+    document.getElementById('small-popup-save').addEventListener('click', function() {
+        // Đóng bảng nhỏ khi bấm nút Close
+        document.getElementById('small-popup-container').style.display = 'none';
+        let payload = {
+            questionId: questionIdInput.value.trim(),
+            text: text_small.value,
+        };
+
+        // Gửi payload dạng JSON
+        fetch('http://127.0.0.1:8000/save-csv-small', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload)
+        }).then(response => {
+            if (response.ok) {
+                alert('success!');
+            } else {
+                alert('Error saving data!');
+            }
+        }).catch(err => {
+            alert('Error sending CSV data: ' + err); 
+            console.error('Error sending CSV data:', err);
+        });
+    });
+
+    // Đóng popup khi nhấp ra ngoài
+    popupOverlay.addEventListener('click', function() {
+        popupContainer.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+    cancel.addEventListener('click', function() {
+        popupContainer.style.display = 'none';
+        popupOverlay.style.display = 'none';
     });
 }
